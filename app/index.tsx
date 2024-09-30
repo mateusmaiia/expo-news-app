cf4b468489b270660749b4d9a481a541554e1495
Maia <matteusmaia889@gmail.com>
2024-09-29 21:53:32 -0300

    feat: backgorund, title, description custom home page

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,23 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { ImageBackground, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import React from "react";
 import { useRouter } from "expo-router";
+import { Colors } from "@/constants/Colors";
 
 const Page = () => {
   const router = useRouter();
 
   return (
     <View style={styles.container}>
-      <Text>Welcome Page</Text>
-      <TouchableOpacity onPress={() => router.replace("/(tabs)")}>
-        <Text>Go to Home Screen</Text>
-      </TouchableOpacity>
+      <ImageBackground source={require("@/assets/images/getting-started.jpg")} style={{flex: 1}} resizeMode="cover" >
+        <View style={styles.wrapper}>
+          <Text style={styles.title}>Stay Updated!</Text>
+          <Text style={styles.description}>Get breaking news and personalized updates directly to your feed.</Text>
+          <TouchableOpacity onPress={() => router.replace("/(tabs)")}>
+            <Text>Go to Home Screen</Text>
+          </TouchableOpacity>
+        </View>
+      </ImageBackground >  
+    
     </View>
   );
 };
@@ -20,7 +27,30 @@ export default Page;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
   },
+  wrapper:{
+    flex: 1,
+    justifyContent: "flex-end",
+    paddingBottom: 50,
+    paddingHorizontal: 30,
+    gap: 10,
+    backgroundColor: "rgba(0,0,0,0.5)",
+  },
+  title:{
+    color: Colors.white,
+    fontSize: 24,
+    fontWeight: '600',
+    textAlign: "center",
+    lineHeight: 36,
+    letterSpacing: 1.5,
+  },
+  description:{
+    color: Colors.white,
+    fontSize:16,
+    fontWeight: '500',
+    letterSpacing: 1.2,
+    lineHeight: 22,
+    textAlign: "center",
+    
+  }
 });
