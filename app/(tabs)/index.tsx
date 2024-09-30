da9c39e3b83e8ad9429e0ee5b0612f616c733f04
Maia <matteusmaia889@gmail.com>
2024-09-29 22:50:26 -0300

    feat: safeArea top space

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,11 +1,14 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { useSafeAreaInsets } from 'react-native-safe-area-context'
 
 type Props = {}
 
 const Page = (props: Props) => {
+  const {top: safeTop} = useSafeAreaInsets()
+
   return (
-    <View style={styles.container}>
+    <View style={[styles.container, {paddingTop: safeTop}]}>
       <Text>Home Screen</Text>
     </View>
   )
